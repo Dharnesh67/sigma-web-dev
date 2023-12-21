695d015b25288d18fe3b7cc6eaee75885605920a
Dharmesh <raikwardharmesh67@gmail.com>
2023-12-21 22:30:18 +0530

    m

--- Js/v74_EVENTS/script.js
+++ Js/v74_EVENTS/script.js
@@ -1,5 +1,18 @@
 let btn = document.getElementById("btn")
-btn.addEventListener("click", () => {
+btn.addEventListener("dblclick", () => {
     btn.style.backgroundColor = "red"
     document.querySelector(".box").innerHTML="Dont touch that button"
+})
+btn.addEventListener("click", () => {
+    btn.style.backgroundColor = "green"
+})
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+btn.addEventListener("contextmenu", () => {
+   alert("why did u right clicked")
+})
+btn.addEventListener("mousemove", () => {
+    btn.style.backgroundColor = "lightgreen"
+})
+btn.addEventListener("mouseout", () => {
+    btn.style.backgroundColor = "lightgreen"
 })
---
+++ Js/v75/script.js
@@ -0,0 +1,13 @@
+function call_back(){
+     
+}
+console.log("hello")
+console.log("hello")
+setTimeout(() => {
+    console.log("timeout")
+}, 0);
+setTimeout(() => {
+    console.log("timeout2")
+}, 0);
+
+console.log("hello")
