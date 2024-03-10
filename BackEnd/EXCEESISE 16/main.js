3bd4527bc9417a452b027f229c71c317bc0675e2
Dharmesh <raikwardharmesh67@gmail.com>
2024-03-10 13:57:20 +0530

    hello

--- BackEnd/EXCEESISE 16/main.js
+++ BackEnd/EXCEESISE 16/main.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {    
-    res.send('Hello World!');
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => { 
+    let html=` Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, libero cupiditate voluptates cum ab eaque aspernatur esse vel. Earum ducimus nesciunt optio fuga quidem fugiat consequuntur blanditiis magni aut. Fugit?`;   
+    res.render('index', {foo: 'World',html:html});
 } 
 );
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+//github.com/mde/ejs/wiki/Using-EJS-with-Express
