f30165c21b8b959275dd9d636fc687ba0594191c
Dharmesh <raikwardharmesh67@gmail.com>
2024-05-12 00:15:17 +0530

    ,sd;cav

--- React/Projects/todolist/c.cpp
+++ React/Projects/todolist/c.cpp
@@ -1,77 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define pii pair<int, int>
-#define pll pair<long long, long long>
-#define vi vector<int>
-#define vll vector<long long>
-#define mii map<int, int>
-#define si set<int>
-#define sc set<char>
 
-#define f(i, s, e) for (long long int i = s; i < e; i++)
-#define cf(i, s, e) for (long long int i = s; i <= e; i++)
-#define rf(i, e, s) for (long long int i = e - 1; i >= s; i--)
-#define pb push_back
-#define eb emplace_back
-int count = 0;
-bool printsubsequencesWithsumk(vector<int> &v, int i, int n, int k, int sum, vector<int> &ans)
+class Solution
 {
-    if (i >= n)
+public:
+    static bool compare(pair<double, int> &a, pair<double, int> &b)
     {
-        if (sum == k)
+        return a.first < b.first;
+    }
+    int maxPointsInsideSquare(vector<vector<int>> &points, string s)
+    {
+        unordered_set<char> st;
+        priority_queue<pair<double, int>, vector<pair<double, int>>, decltype(&compare)> pq(compare);
+        for (int i = 0; i < points.size(); i++)
         {
-            for (int x : ans)
-                cout << x << " ";
-            cout << endl;
+            double distance = sqrt(points[i][0] * points[i][0] + points[i][1] * points[i][1]);
+            pq.push({distance, i});
         }
-        return sum == k;
+        int count = 0;
+        int maxx = INT_MIN;
+        int maxy = INT_MIN;
+        while (!pq.empty())
+        {
+            auto x = pq.top();
+            pq.pop();
+            maxx = max(points[x.second][0], maxx);
+            maxy = max(points[x.second][1], maxy);
+            if (!st.count(s[x.second]))
+            {
+                st.insert(s[x.second]);
+            }
+            else
+            {
+                for (int i = 0; i < points.size(); i++)
+                {
+                    if (maxx>points[i][0] && maxy>points[i][1] && st.count(s[i])){
+                            count++;
+                    }
+                }
+                break;
+            }
+        }
+        return count;
     }
-    if (sum > k)
-    {
-        return false;
-    }
-    if (sum == k)
-    {
-        for (int x : ans)
-            cout << x << " ";
-        cout << endl;
-        return true;
-    }
-    ans.push_back(v[i]);
-    bool take =printsubsequencesWithsumk(v, i + 1, n, k, sum + v[i], ans);
-    if(take) return true;
-    ans.pop_back();
-    bool notake=printsubsequencesWithsumk(v, i + 1, n, k, sum, ans);
-    if(notake) return true;
-}
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    // int t;
-    // cin>>t;
-    // while(t--){
-    //     int n;
-    //     cin>>n;
-    //     int a[n];
-    //     f(i,0,n){
-    //         cin>>a[i];
-    //     }
-    //     int ans=0;
-    //     f(i,0,n-1){
-    //         int mn=min(a[i],a[i+1]);
-    //         int mx=max(a[i],a[i+1]);
-    //         while(2*mn<mx){
-    //             mn*=2;
-    //             ans++;
-    //         }
-    //     }
-    //     cout<<ans<<endl;
-    // }
-    vector<int> v = {1, 2, 1, 2, 1};
-    vector<int> ans;
-    printsubsequencesWithsumk(v, 0, v.size(), 3, 0, ans);
-    return 0;
-}
+};
